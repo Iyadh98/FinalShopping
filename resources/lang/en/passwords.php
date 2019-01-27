0900a77e897d2d6b539b393658969464b59bd6cd
Iyadh98 <iyadhkhalfallah98@gmail.com>
2019-01-27 12:39:16 +0100

    Error messages + Front Login Register

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Le mot de passe doit avoir au moins 6 caractères et doit correspondre a la confirmation',
+    'reset' => 'Votre mot de passe a été réinitialisé!',
+    'sent' => 'Nous vous avons envoye un email contenant le lien de réinitialisation du mot de passe!',
+    'token' => 'Le mot de passe est invalide',
+    'user' => "Nous ne pouvons pas trouver un utilisateur avec cette adresse email",
 
 ];
